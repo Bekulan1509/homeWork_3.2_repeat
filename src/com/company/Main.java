e1f940a0f0d569907b07c7b0309a4e2a905a56fc
Bekulan <totonya1509@bk.ru>
2019-11-18 23:46:56 +0600

    adsd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static void main(String[] args)  {
         BankAccount bankAccount = new BankAccount();
-        System.out.println("у вас насчету "+bankAccount.deposit(10000));
+        System.out.println("у вас насчету "+bankAccount.deposit(27000));
         while (true) {
             try {
                 System.out.println("остаток на счету "+bankAccount.withDraw(6000));
             } catch (Exception e){
                 System.out.println(e.getMessage());
-                System.out.println("Вы успешно сняли 4000 сом со счета");
+                System.out.println("Вы успешно сняли деньги "+bankAccount.getAmount() +" сомов со счета");
                 bankAccount.takeMoney();
                 System.out.println(bankAccount.getAmount());
                 break;
